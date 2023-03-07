a65d380b9d3ffc3a5cc4872784f2eaac585155d7
barron13 <nicolas.barroga13@gmail.com>
2023-03-06 21:00:52 -0800

    Added array of string values for DegreeProgram

--- degree.h
+++ degree.h
@@ -1,1 +1,5 @@
+//Define DegreeProgram class
 enum class DegreeProgram {SECURITY = 3, NETWORK = 2, SOFTWARE = 1, UNDETERMINED = 0};
+
+//Define array used to retrieve string value of DegreeProgram class
+static const std::string degreeStrings[] = { "SECURITY", "NETWORK", "SOFTWARE", "UNDETERMINED"};

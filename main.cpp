ccb76e9dc404c4c2d3a1624cd71e21c17c0d1a79
barron13 <nicolas.barroga13@gmail.com>
2023-03-06 19:43:10 -0800

    Added studentData[] into main function

--- main.cpp
+++ main.cpp
@@ -7,4 +7,13 @@ using namespace std;
 
 int main() {
 
+// Input studentData from task scenario
+
+    const string studentData[] =
+    {"A1, John, Smith, John1989@gm ail.com, 20, 30, 35, 40, SECURITY",
+     "A2, Suzan, Erickson, Erickson_1990@gmailcom, 19, 50, 30, 40 ,NETWORK",
+     "A3, Jack,Napoli, The_lawyer99yahoo.com, 19, 20, 40, 33, SOFTWARE",
+     "A4, Erin, Black, Erin.black@comcast.net, 22, 50,58, 40, SECURITY",
+     "A5, Nicolas,Barroga,nbarrog@wgu.edu, 26, 10, 15, 30, SOFTWARE"};
+     
 }
